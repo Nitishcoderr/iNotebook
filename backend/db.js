312e14e8f3516d1c85531a7cc30988cd798aa0c1
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-05-17 12:53:00 +0530

    video-45-completed

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // establishing mongodb server
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true";
 
 connectToMongo().catch(err => console.log(err));
 
