1e00b5d76c2ed707591435dccd1902b0f22fa7aa
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-05-18 17:44:33 +0530

    video-51-completed

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,25 +3,24 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UsereSchema = new Schema({
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    date:{
-        type:Date,
-        default:Date.now
+    date: {
+        type: Date,
+        default: Date.now
     },
 });
 
-const User = mongoose.model('user',UsereSchema)
+const User = mongoose.model('user', UsereSchema)
 User.createIndexes()
 module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,58 +1,114 @@
-const express = require('express')
-const User = require('../models/User')
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 // bcryptjs using
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 // json web token
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // express vallidator
-const { body, validationResult } = require('express-validator');
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
 
-
-const JWT_SECRET = 'nitishisagoodboy@12434'
-// creating a user uding : post "api/auth/createuser".no login required
-
-router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be at least 5 character').isLength({ min: 5 })
-], async (req, res) => {
+const JWT_SECRET = "nitishisagoodboy@12434";
+//ROUTES - 1 creating a user using : post "api/auth/createuser".no login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be at least 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
     // if there are error, return BAD request and the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
     // check wheather the user with this email exists already
     try {
-        let user = await User.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exists" })
-        }
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
 
-        const salt = await bcrypt.genSalt(10);
-        secPass = await bcrypt.hash("req.boby.password", salt); 
-        // create a user 
-        user = await User.create({
-            name: req.body.name,
-            password: secPass,
-            email: req.body.email,
-        })
-        // using jwebtoken
-        const data = {
-            user:{
-                id:user.id
-            }
-        }
-        const authtoken = jwt.sign(data,JWT_SECRET);
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+      // create a user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+      // using jwebtoken
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
 
-        // res.json(user)
-        res.json({authtoken})
-
+      // res.json(user)
+      res.json({ authtoken });
     } catch (error) {
-        console.error(error.message)
-        res.status(500).send("Some error occured")
-    } {
+      console.error(error.message);
+      res.status(500).send("Internal server occured");
     }
-})
+  }
+);
 
-module.exports = router
+//ROUTES - 2 loging a user using : post "api/auth/login".no login required
+router.post("/login",[
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blanked").exists(),
+  ],
+  async (req, res) => {
+    // if there are error, return BAD request and the error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server occured");
+    }
+  }
+); 
+//ROUTES - 3 get a loging user detail using : POST "api/auth/getuser".login required
+router.post("/getuser",fetchuser,async (req, res) => {
+try {
+    userId = req.user.id
+    const user = await User.findById(userId).select("-password")
+    res.send(user)
+    
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server occured");
+  }
+})
+module.exports = router;
