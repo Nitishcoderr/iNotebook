49f0406fa867c9e95789535fa65858550c32ce9a
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-05-31 21:50:29 +0530

    Completed

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 // establishing mongodb server
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // For mongo compass
 // const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true"; 
 // for mongo atlas
-const mongoURI = "mongodb+srv://nitishcoderr:Nitish25@cluster0.icw48xp.mongodb.net/";
+const mongoURI = process.env.MONGODB_COMPASS;
+
 
 connectToMongo().catch(err => console.log(err));
 
